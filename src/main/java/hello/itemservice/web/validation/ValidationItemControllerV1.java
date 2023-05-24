671d7391361631cf0a811b40608b6e9142343093
kimjunoh <kimjo8317@gmail.com>
2023-05-24 15:55:13 +0900

    first commit

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
@@ -44,18 +44,19 @@ public class ValidationItemControllerV1 {
 
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
+
         //검증 오류 결과를 보관
-        Map<String, String> errors = new HashMap<>();//검증결과를 생성하기때문에 Null이 아님
+        Map<String, String> errors = new HashMap<>();
 
         //검증 로직
         if (!StringUtils.hasText(item.getItemName())) {
             errors.put("itemName", "상품 이름은 필수입니다.");
         }
         if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
-            errors.put("price", "가격은 1,000원 ~ 1,000,000원 까지 허용됩니다");
+            errors.put("price", "가격은 1,000 ~ 1,000,000 까지 허용합니다.");
         }
-        if (item.getQuantity() == null || item.getQuantity() > 9999) {
-            errors.put("quantity", "수량은 최대 9,999개 까지 허용합니다.");
+        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
+            errors.put("quantity", "수량은 최대 9,999 까지 허용합니다.");
         }
 
         //특정 필드가 아닌 복합 룰 검증
@@ -68,12 +69,12 @@ public class ValidationItemControllerV1 {
 
         //검증에 실패하면 다시 입력 폼으로
         if (!errors.isEmpty()) {
-            log.info("errors = {}", errors);
+            log.info("errors = {} ", errors);
             model.addAttribute("errors", errors);
             return "validation/v1/addForm";
         }
 
-        //성공로직
+        //성공 로직
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
